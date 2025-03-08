9616db826b0cfc76e03b44c70e666af4a05f5274
deep <Deepmangukiya29@gmail.com>
2025-03-08 12:43:52 +0530

    origin added

--- InstaVideo_Downloader/settings.py
+++ InstaVideo_Downloader/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-laog+on#gypuo&#+^99q27a(vsto-mo4gv-bvrjxxa4gvevf7d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
